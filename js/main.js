4c37c370aea39c884f4604877262fcf9155c7e1e
joric <joric@users.noreply.github.com>
2026-06-19 16:01:19 +0500

    updated files

--- js/main.js
+++ js/main.js
@@ -187,7 +187,7 @@ function processImage(img) {
   // mask should be relaxed because of rgb rounding errors
   // looks like it doesn't work reliably, need specific bruteforcer
   // that will bruteforce all colors and filter by rgb results
-  mask='11111111111111110000000000000000';
+  //mask='11111111111111110000000000000000';
   mask='1111111111111111000000000f808080';
 
   // also there's something shady with pixel mask, I am not getting joric/852547 at all
