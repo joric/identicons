e667dad0a91e7d1944a444c136f82177f7294622
joric <joric@users.noreply.github.com>
2026-08-04 18:40:33 +0500

    updated files

--- js/main.js
+++ js/main.js
@@ -630,7 +630,7 @@ function onUserIdChangedDebounced() {
   generate();
 
   clearTimeout(autoFetchTimer);
-  //autoFetchTimer = setTimeout(fetchUsername, AUTO_FETCH_DEBOUNCE_MS); // commented out for now, API limit is too low
+  autoFetchTimer = setTimeout(fetchUsername, AUTO_FETCH_DEBOUNCE_MS); // comment out, if API limit is too low
 }
 
 function onUserIdInput() {
